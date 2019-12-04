aca47349d1ebf781cb98b1a7d3098869b471601f
afoyer <lemimic213@gmail.com>
2019-12-04 09:59:29 -0700

    Swipeable Tabs!

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import {View, Text} from 'react-native'
-import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {createMaterialTopTabNavigator} from 'react-navigation-tabs'
 
 import HomeScreen from '../screens/HomeScreen'
 import Settings from '../screens/Settings'
 
 
-const TabNavigator = createBottomTabNavigator({
+const TabNavigator = createMaterialTopTabNavigator({
     One: HomeScreen,
     Two: Settings
+},{
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarPosition :'bottom',
+    tabBarOptions: {
+      style: {
+        backgroundColor: '#607d8b',
+      },
+      activeBackgroundColor: '#b0bec5',
+      inactiveBackgroundColor: '#78909c',
+      activeTintColor: '#fff',
+      inactiveTintColor: '#9B9B9B',
+      indicatorStyle: {
+        height: '100%',
+        backgroundColor: '#b0bec5'
+      }
+    }
 });
 export default TabNavigator;
--- screens/Settings.js
+++ screens/Settings.js
@@ -20,6 +20,7 @@ export default class Settings extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        alignItems :'center',
         justifyContent: 'center',
     },
 })
