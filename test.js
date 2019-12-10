9be2b6d6fadf509f4deba5ce27a0e1f7cc3a9c50
drewshki <d_shippey@coloradocollege.edu>
2019-12-10 15:26:45 -0700

    Exportable test
    nobody watches these

--- test.js
+++ test.js
@@ -1,19 +1,5 @@
 var mysql = require('mysql');
 
-
-
-//function myFunc( con ) {
-//  con.connect(function(err) {
-//    if (err) throw err;
-//    console.log("Connected!");
-//    con.query("CREATE DATABASE IF NOT EXISTS eventdb", function (err, result) {
-//      if (err) throw err;
-//      console.log("Database created");
-//      con.end();
-//    });
-//    });
-//}
-
 function createDatabase( con ) {
     con.query("CREATE DATABASE IF NOT EXISTS eventdb", function (err, result) {
       if (err) throw err;
@@ -56,30 +42,32 @@ function createTagsTable( con ) {
     });
 }
 
-function getConnection(){
-    var mysql = require('mysql');
-    var con = mysql.createConnection({
-        host     : "eventsdatabasegood.ckdvrhiblj4u.us-east-2.rds.amazonaws.com",
-        user     : "Drewship",
-        password : "cupcake1234",
-        database : "eventdb"
-    });
-    return con
-}
+const createConnection = function(){
+                            var mysql = require('mysql');
+                            var con = mysql.createConnection({
+                                host     : "eventsdatabasegood.ckdvrhiblj4u.us-east-2.rds.amazonaws.com",
+                                user     : "Drewship",
+                                password : "cupcake1234",
+                                database : "eventdb"
+                            });
+                            console.log("Connected to eventdb")
+                            return con
+                        }
 
-function createEvent( con , param_list ){
-    sql_add_event = "INSERT INTO events (Event_ID , Student_ID , \
+const createEvent = function( con , param_list ){
+                        con.connect()
+                        sql_add_event = "INSERT INTO events (Event_ID , Student_ID , \
                                          Event_Name , Event_Location, \
                                          Event_Description , Event_Date, \
                                          Event_Start , Event_End , \
                                          Poster , CCSGA_Approved) \
-                     VALUES( ? , ? , ? , ? , ? , ?, ? , ? , ? ,?)"
+                        VALUES( ? , ? , ? , ? , ? , ?, ? , ? , ? ,?)"
 
 
-    con.query(sql_add_event, param_list,  function (err, result) {
-          if (err) throw err;
-          console.log("Event " + Event_ID + " added");
-     });
+                        con.query(sql_add_event, param_list,  function (err, result) {
+                              if (err) throw err;
+                              console.log("Event " + Event_ID + " added");
+                         });
 }
 
 exports.getAllEvents = function( con , callBack){
@@ -97,16 +85,19 @@ exports.getAllEvents = function( con , callBack){
 
 function main() {
 
-    var con = getConnection()
-    con.connect()
-    createDatabase(con)
-    createEventsTable(con)
-    createTagsTable( con )
-    p_list = [5 , 123456 , "test" , "somewhere" , "something" ,"now" , 1 , 2 , null , 1]
+    var con = createConnection()
+    //createDatabase(con)
+    //createEventsTable(con)
+    //createTagsTable( con )
+    p_list = [1223 , 123456 , "test" , "somewhere" , "something" ,"now" , 1 , 2 , null , 1]
     createEvent( con , p_list )
     //var result = getEvents( con );
-   con.end
+    con.end
 
 }
 
 main()
+
+module.exports ={
+    createConnection
+}
